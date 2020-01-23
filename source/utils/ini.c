6f77b5924c8d13bef699e09c39f126b3a4e952fa
Such Meme, Many Skill <owen.vanrijn@gmail.com>
2020-01-24 00:10:37 +0100

    Improve ini parsing to work with (one of) the atmosphere configs

--- source/utils/ini.c
+++ source/utils/ini.c
@@ -4,53 +4,76 @@
 #include <stdlib.h>
 #include "ini.h"
 
-char key[255], value[255];
+char section[255], key[255], value[255];
 
 void SplitString(char *in, char split){
     int i = 0, pos = 0, length = strlen(in);
 
     while (i < length && in[i] != split){
-        key[i] = in[i];
-        i++;
+        if (in[i] == ' '){
+            i++;
+            continue;
+        }
+
+        key[pos] = in[i];
+        pos++, i++;
     }
-    key[i] = '\0';
-    i++;
+
+    key[pos] = '\0';
+    pos = 0, i++;
 
     while (i < length){
+        if (in[i] == ' '){
+            i++;
+            continue;
+        }
+            
         value[pos] = in[i];
         i++, pos++;
     }
 
+    if (pos > 0)
+        if (value[pos - 1] == '\n')
+            value[pos - 1] = '\0';
+
     value[pos] = '\0';
+}
+
+void RemoveBrackets(char *in){
+    strcpy(section, in + 1);
+    for (int i = 0; i < strlen(section); i++){
+        if (section[i] == ']'){
+            section[i] = '\0';
+            break;
+        }
+    }
 }
 
 int ini_parse(const char* filename, ini_handler handler) {
     FILE *file;
-    char section[255], temp[255], c;
+    char temp[255], c;
     file = fopen(filename, "r");
 
     while (1){
         if ((c = fgetc(file)) == 255)
             break;
 
-        printf("c=%c(%d)\n", c, (int)c);
-
         switch (c){
             case '\n':
                 break;
             case ';':
-                fscanf(file, "%s\n", key);
+                fgets(temp, 255, file);
                 break;
             case '[':
                 fseek(file, -1, SEEK_CUR);
-                fscanf(file, "[%s]\n", section);
-                section[strlen(section) - 1] = '\0';
+                fgets(temp, 255, file);
+                RemoveBrackets(temp);
                 break;
             case (char)255:
                 goto out;
             default:
                 fseek(file, -1, SEEK_CUR);
-                fscanf(file, "%s\n", temp);
+                fgets(temp, 255, file);
                 SplitString(temp, '=');
                 handler(section, key, value);
                 break;
